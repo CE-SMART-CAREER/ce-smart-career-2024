38edd620928e0b70ec24baf01aab446e2d72810d
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-10-24 18:18:16 +0700

    fix: change bg modal color

--- src/modules/home/_components/company-logo.tsx
+++ src/modules/home/_components/company-logo.tsx
@@ -39,8 +39,8 @@ export function CompanyLogo({ name, logo, type, description }: Props) {
 
       <DialogPortal>
         <DialogOverlay className="fixed inset-0 bg-black/50 data-[state=closed]:animate-[dialog-overlay-hide_200ms] data-[state=open]:animate-[dialog-overlay-show_200ms]" />
-        <DialogContent className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-linear-soft-gray p-8 text-gray-900 shadow backdrop-blur-3xl data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms] [&>button]:hidden">
-          <div className="flex flex-col justify-between gap-4 text-white">
+        <DialogContent className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow backdrop-blur-3xl data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms]">
+          <div className="flex flex-col justify-between gap-4">
             <Image
               src={logo}
               alt={name}
@@ -49,11 +49,11 @@ export function CompanyLogo({ name, logo, type, description }: Props) {
               className="mx-auto w-full max-w-[30ch] object-contain"
             />
             <DialogTitle>
-              <div className="text-md pt-4">{name}</div>
+              <div className="text-md mt-4">{name}</div>
               <div className="pt-2 text-orange-300">{type}</div>
             </DialogTitle>
             <DialogDescription>
-              <div className="text-white">{description}</div>
+              <div>{description}</div>
             </DialogDescription>
           </div>
         </DialogContent>
