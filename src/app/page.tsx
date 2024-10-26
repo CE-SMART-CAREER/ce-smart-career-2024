e56619f2204bab660388bbe64c5fe0ca1bb73b48
ktunchar <maikittitee1615@gmail.com>
2024-10-26 21:07:57 +0700

    feat: clean navbar and restructure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { CompanyLogos } from '@/modules/home/_sections';
-import NavBar from './navbar';
+import NavBar from '../shared/components/navbar/navbar';
+import nav_tabs from '@/shared/components/navbar/_constants/nav-tabs';
 
 export default function Home() {
   return (
     <>
       <header>
-        <NavBar />
+        <NavBar items={nav_tabs} />
       </header>
       <main>
         <CompanyLogos />
