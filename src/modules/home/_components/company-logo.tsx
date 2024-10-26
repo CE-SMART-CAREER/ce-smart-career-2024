e9e401ecdbbf28917ad86b16f310a2981333c094
Chaksawat <88503339+Nonene2546@users.noreply.github.com>
2024-10-26 16:45:48 +0700

    feat: shrink dialog to not touch the edge when screen is small and remove some unnecessary css

--- src/modules/home/_components/company-logo.tsx
+++ src/modules/home/_components/company-logo.tsx
@@ -38,15 +38,15 @@ export function CompanyLogo({ name, logo, type, description }: Props) {
       </DialogTrigger>
 
       <DialogPortal>
-        <DialogOverlay className="fixed inset-0 bg-black/50 data-[state=closed]:animate-[dialog-overlay-hide_200ms] data-[state=open]:animate-[dialog-overlay-show_200ms]" />
-        <DialogContent className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow backdrop-blur-3xl data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms]">
+        <DialogOverlay className="bg-black/50" />
+        <DialogContent className="max-w-[min(28rem,_90vw)] rounded-md bg-white text-black sm:max-w-md">
           <div className="flex flex-col justify-between gap-4">
             <Image
               src={logo}
               alt={name}
               width={164}
               height={61}
-              className="mx-auto w-full max-w-[30ch] object-contain"
+              className="mx-auto h-[164px] w-full max-w-[30ch] object-contain"
             />
             <DialogTitle>
               <div className="text-md mt-4">{name}</div>
