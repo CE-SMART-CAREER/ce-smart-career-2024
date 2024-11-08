27a6015522cd9b7e7698b26ac4ee6459f76bfb74
Kachen <106764256+Roshanen@users.noreply.github.com>
2024-11-08 23:59:34 +0700

    FEAT | better responsive seminar section (#25)

--- src/modules/home/_sections/seminar.tsx
+++ src/modules/home/_sections/seminar.tsx
@@ -8,8 +8,8 @@ export default async function Seminar() {
   const seminarList = await getSeminarsGroupedByDay();
 
   return (
-    <article className="relative mx-auto flex flex-col bg-black pb-20">
-      <div className="relative w-full">
+    <article className="relative mx-auto flex h-svh flex-col bg-black">
+      <div className="absolute h-full w-full">
         <Image
           src="/assets/illustrations/cloud.png"
           alt="cloud-png"
@@ -20,8 +20,8 @@ export default async function Seminar() {
       </div>
       <div className="absolute left-0 top-0 h-72 w-72 rounded-full bg-orange-200 opacity-30 blur-2xl"></div>
 
-      <div className="relative mx-auto flex w-10/12 flex-col md:w-3/4">
-        <h2 className="w-full text-3xl font-bold md:text-4xl">
+      <div className="relative mx-auto flex w-10/12 flex-col pt-[8%] md:w-3/4">
+        <h2 className="w-full text-3xl font-bold sm:text-3xl md:text-4xl">
           กำหนดการสัมมนา
         </h2>
         {seminarList.length <= 0 && (
@@ -31,17 +31,17 @@ export default async function Seminar() {
         )}
 
         <Tabs defaultValue="day1" className="flex flex-col gap-2">
-          <TabsList className="mx-10 my-6 flex w-fit flex-row flex-wrap gap-4 md:gap-6 lg:gap-20">
+          <TabsList className="mx-0 my-4 flex w-fit flex-row flex-wrap gap-4 sm:mx-10 sm:my-6 md:mx-2 md:gap-8 lg:gap-20">
             {seminarList.map((seminarSlot, index) => (
               <TabsTrigger
                 key={index + 1}
                 value={`day${index + 1}`}
-                className="flex flex-row flex-wrap items-baseline gap-5 py-3 text-xl text-gray-400 transition-colors duration-300 hover:text-white data-[state=active]:border-b-2 data-[state=active]:border-b-orange-300 data-[state=active]:text-orange-300 md:text-2xl"
+                className="flex flex-row flex-wrap items-baseline gap-2 text-xl text-gray-400 transition-colors duration-300 hover:text-white data-[state=active]:border-b-2 data-[state=active]:border-b-orange-300 data-[state=active]:text-orange-300 sm:gap-5 sm:py-3 md:text-2xl"
               >
-                <span className="text-xl font-bold md:text-2xl">
+                <span className="text-lg font-bold sm:text-xl md:text-2xl">
                   Day {index + 1}{' '}
                 </span>
-                <span className="text-md md:text-xl">
+                <span className="sm:text-lg md:text-xl">
                   {fDate(seminarSlot.date, formatStr.longDate)}
                 </span>
               </TabsTrigger>
