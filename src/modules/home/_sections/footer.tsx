4debf36f764d659451fb11e90c669e417adeee5f
ktunchar <maikittitee1615@gmail.com>
2024-11-08 23:37:21 +0700

    feat: adjust css to navbar and footer

--- src/modules/home/_sections/footer.tsx
+++ src/modules/home/_sections/footer.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@iconify/react';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-200 p-8 text-center">
+    <footer className="bg-black-200 bg-linear-orange-gray p-8 text-center">
       <h2 className="mb-4 text-lg font-bold">ติดตามข่าวสาร</h2>
       <div className="flex justify-center space-x-8">
         <Icon icon="fa:instagram" className="text-3xl" />
