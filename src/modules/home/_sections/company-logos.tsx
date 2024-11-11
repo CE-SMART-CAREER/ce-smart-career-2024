7606400a0129c2ed7d8d3eafe25e1319913cf079
Chaksawat <88503339+Nonene2546@users.noreply.github.com>
2024-11-12 01:40:59 +0700

    move content in company-logos into container

--- src/modules/home/_sections/company-logos.tsx
+++ src/modules/home/_sections/company-logos.tsx
@@ -18,23 +18,25 @@ export default function CompanyLogos({ companies, selectedCompanyId }: Props) {
       <div id="companies" className="anchor-holder invisible relative p-4">
         $
       </div>
-      <section className="bg-linear-orange-gray px-0 py-10 text-white sm:px-10">
-        <h2 className="mb-10 text-center text-3xl font-semibold">
-          บริษัทที่เข้าร่วม
-          <span className="visuallyhidden">กิจกรรม CE smart career 2024</span>
-        </h2>
+      <div className="container relative mx-auto xl:max-w-screen-xl">
+        <section className="bg-linear-orange-gray px-0 py-10 text-white sm:px-10">
+          <h2 className="mb-10 text-center text-3xl font-semibold">
+            บริษัทที่เข้าร่วม
+            <span className="visuallyhidden">กิจกรรม CE smart career 2024</span>
+          </h2>
 
-        <div className="grid grid-cols-2 gap-6 px-4 sm:grid-cols-3 sm:px-6 md:grid-cols-4 lg:px-8">
-          {companies?.map((company: Company, index: number) => (
-            <CompanyLogo
-              key={index}
-              name={company?.name}
-              logo={company?.logo}
-              companyId={company?.Id}
-            />
-          ))}
-        </div>
-      </section>
+          <div className="grid grid-cols-2 gap-6 px-4 sm:grid-cols-3 sm:px-6 md:grid-cols-4 lg:px-8">
+            {companies?.map((company: Company, index: number) => (
+              <CompanyLogo
+                key={index}
+                name={company?.name}
+                logo={company?.logo}
+                companyId={company?.Id}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
       {selectedCompany && (
         <CompanyInfoModal
           name={selectedCompany.name}
