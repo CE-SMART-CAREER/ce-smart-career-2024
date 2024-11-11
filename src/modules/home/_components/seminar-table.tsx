299e6b30d692d3fb688b8b76d5f782ea717aab97
Kachen <numesattahip@hotmail.com>
2024-11-11 18:25:50 +0700

    style: add seminar table fade animation

--- src/modules/home/_components/seminar-table.tsx
+++ src/modules/home/_components/seminar-table.tsx
@@ -8,16 +8,16 @@ type Props = {
 export function SeminarTable({ seminars }: Props) {
   return (
     <div className="h-[60svh] overflow-auto rounded-lg">
-      <table className="w-full table-auto border-collapse rounded-lg border-none bg-linear-gray-orange">
+      <table className="w-full table-auto border-collapse animate-fade rounded-lg border-none bg-linear-gray-orange">
         <thead>
           <tr>
-            <th className="md:pl-15 mx-6 w-1/3 border-none py-3 pl-10 text-left md:py-6 lg:py-8 lg:pl-20">
+            <th className="md:pl-15 mx-6 w-1/3 animate-fade border-none py-3 pl-10 text-left md:py-6 lg:py-8 lg:pl-20">
               เวลา
             </th>
-            <th className="w-1/3 border-none py-3 pl-10 text-left md:py-6 lg:py-8 lg:pl-20">
+            <th className="w-1/3 animate-fade border-none py-3 pl-10 text-left md:py-6 lg:py-8 lg:pl-20">
               ห้องที่ 1 (001)
             </th>
-            <th className="w-1/3 border-none py-3 pl-10 text-left md:py-6 lg:py-8 lg:pl-20">
+            <th className="w-1/3 animate-fade border-none py-3 pl-10 text-left md:py-6 lg:py-8 lg:pl-20">
               ห้องที่ 2 (002)
             </th>
           </tr>
@@ -25,13 +25,13 @@ export function SeminarTable({ seminars }: Props) {
         <tbody>
           {seminars.map((seminar, index) => (
             <tr key={index}>
-              <td className="md:pl-15 mx-6 w-1/3 border-none py-3 pl-10 md:py-6 lg:py-8 lg:pl-20">
+              <td className="md:pl-15 mx-6 w-1/3 animate-fade border-none py-3 pl-10 md:py-6 lg:py-8 lg:pl-20">
                 {`${fTime(seminar.startAt, formatStr.time24Hr)}-${fTime(seminar.endAt, formatStr.time24Hr)}`}
               </td>
-              <td className="w-1/3 border-none py-3 pl-10 md:py-6 lg:py-8 lg:pl-20">
+              <td className="w-1/3 animate-fade border-none py-3 pl-10 md:py-6 lg:py-8 lg:pl-20">
                 {seminar.room1}
               </td>
-              <td className="w-1/3 border-none py-3 pl-10 md:py-6 lg:py-8 lg:pl-20">
+              <td className="w-1/3 animate-fade border-none py-3 pl-10 md:py-6 lg:py-8 lg:pl-20">
                 {seminar.room2}
               </td>
             </tr>
