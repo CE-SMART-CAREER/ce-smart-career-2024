e512b2109f906f088fec59b1b6af2a09e3a0bc88
Kachen <numesattahip@hotmail.com>
2024-11-12 20:37:15 +0700

    feat: remove invisible box and add id 'about'

--- src/modules/home/_sections/introduction.tsx
+++ src/modules/home/_sections/introduction.tsx
@@ -29,7 +29,10 @@ function Grid() {
 
 export default function Introduction() {
   return (
-    <div className="relative flex min-h-screen items-center justify-center overflow-hidden p-4">
+    <div
+      id="about"
+      className="relative flex min-h-screen items-center justify-center overflow-hidden p-4"
+    >
       <City />
 
       <div className="relative flex flex-col items-center justify-center md:-mt-32">
